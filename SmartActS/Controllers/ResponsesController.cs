3724f6756db084af00c25a7168860f53caca1816
huuchung <huuchung@TEO>
2017-06-04 14:56:02 +0700

    chung add model

---
+++ SmartActS/Controllers/ResponsesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SmartActS.DataModels;
+
+namespace SmartActS.Controllers
+{
+    public class ResponsesController : Controller
+    {
+        private SmartActSModel db = new SmartActSModel();
+
+        // GET: Responses
+        public ActionResult Index()
+        {
+            return View(db.Responses.ToList());
+        }
+
+        // GET: Responses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // GET: Responses/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Responses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ResponseId,ResponseTitle,RequestId,SupplyId,ResponseTime,Status,PriceSuggest,Description,FileAttachId,categoryId")] Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Responses.Add(response);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(response);
+        }
+
+        // GET: Responses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // POST: Responses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ResponseId,ResponseTitle,RequestId,SupplyId,ResponseTime,Status,PriceSuggest,Description,FileAttachId,categoryId")] Response response)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(response).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(response);
+        }
+
+        // GET: Responses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Response response = db.Responses.Find(id);
+            if (response == null)
+            {
+                return HttpNotFound();
+            }
+            return View(response);
+        }
+
+        // POST: Responses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Response response = db.Responses.Find(id);
+            db.Responses.Remove(response);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
